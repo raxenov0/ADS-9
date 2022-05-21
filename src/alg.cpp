3713ca47b9e6438bf0b25e250428780742a34a6d
Axenov Roman <98100948+raxenov0@users.noreply.github.com>
2022-05-21 15:44:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -15,11 +15,11 @@ BST<std::string> makeTree(const char* filename) {
   if (!in) {
     std::cout << "File error!" << std::endl;
   }
-  while (getline(in, str,' ')) {
+  while (getline(in, str, ' ')) {
     transform(str.begin(), str.end(), str.begin(), tolower);
     for (int i = 0; i < str.length(); i++) {
       if (str[i] == '\0') break;
-      else if(str[i] != '\n' && str[i] != ' ') temp += str[i];
+      else if (str[i] != '\n' && str[i] != ' ') temp += str[i];
     }
     tree.addNode_(temp);
     temp = "";
